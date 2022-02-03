81a81f0a7796b6a25a0ecff53c8eda4e8321e4a9
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2022-02-04 03:17:31 +0530

    Make the TextBox automatically and constantly focused

--- Pages/Note.xaml.cs
+++ Pages/Note.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 using MUXC = Microsoft.UI.Xaml.Controls;
 
@@ -12,6 +14,22 @@ namespace Notes.Pages
         public Note()
         {
             this.InitializeComponent();
+        }
+
+        private void NoteInput_Loading(FrameworkElement sender, object args)
+        {
+            // Auto-focus the note input TextBox as soon as it starts loading.
+            // TextBox is loaded as soon as its parent tab is focused and opened
+            ((TextBox)sender).Focus(FocusState.Programmatic);
+        }
+
+        private void NoteInput_LosingFocus(UIElement sender, LosingFocusEventArgs args)
+        {
+            // Keep the TextBox constantly focused by cancelling
+            // the focus action and not letting the TextBox lose
+            // focus to a different control. Does not cancel, for
+            // example, when the app itself is losing focus, as expected
+            args.TryCancel();
         }
     }
 }
