8e354c0515e7dc08c8e3bea56ba73ed42af01deb
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2022-02-04 00:51:38 +0530

    Make the TabView interactive while keeping the app window mobility intact

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 using MUXC = Microsoft.UI.Xaml.Controls;
@@ -13,6 +14,44 @@ namespace Notes
         public MainPage()
         {
             this.InitializeComponent();
+
+            // Make the TabView interactive while
+            // maintaining the mobility of the app window
+            ChangeTitleBar();
+        }
+
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            if (FlowDirection == FlowDirection.LeftToRight)
+            {
+                CustomDragRegion.MinWidth = sender.SystemOverlayRightInset;
+                ShellTitlebarInset.MinWidth = sender.SystemOverlayLeftInset;
+            }
+            else
+            {
+                CustomDragRegion.MinWidth = sender.SystemOverlayLeftInset;
+                ShellTitlebarInset.MinWidth = sender.SystemOverlayRightInset;
+            }
+
+            CustomDragRegion.Height = ShellTitlebarInset.Height = sender.Height;
+        }
+
+        /// <summary>
+        /// Change the core title bar with a custom title bar to allow
+        /// interaction with TabView extended into the title bar area
+        /// while still having a draggable region to move the app window
+        /// </summary>
+        private void ChangeTitleBar()
+        {
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            // Register a handler for when the size of the overlaid caption control changes.
+            // For example, when the app moves to a screen with a different DPI.
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+
+            // Set new title bar element as a draggable region.
+            Window.Current.SetTitleBar(CustomDragRegion);
         }
 
         private void Tabs_Loading(FrameworkElement sender, object args)
