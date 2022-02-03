0a68c3b9346fcce15571bd9e5f1865a7209a3ff5
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2022-02-04 02:30:18 +0530

    Make the TabView responsive

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -54,19 +54,68 @@ namespace Notes
             Window.Current.SetTitleBar(CustomDragRegion);
         }
 
+        /// <summary>
+        /// Creates a new tab in the given TabView and opens it.
+        /// </summary>
+        /// <param name="tabView">The TabView to add the new tab in</param>
+        private void CreateTab(MUXC.TabView tabView)
+        {
+            var tab = new MUXC.TabViewItem()
+            {
+                Header = "Untitled.txt",
+                IconSource = new MUXC.SymbolIconSource() { Symbol = Symbol.Document }
+            };
+
+            var content = new Frame();
+            tab.Content = content;
+
+            tabView.TabItems.Add(tab);
+            content.Navigate(typeof(Pages.Note));
+
+            // Open the newly created tab
+            tabView.SelectedItem = tab;
+        }
+
+        /// <summary>
+        /// Closes the passed tab in the given TabView.
+        /// Closes the app itself if the last open tab is closed too.
+        /// </summary>
+        /// <param name="tabView">The TabView containing the tab</param>
+        /// <param name="tab">The tab to close</param>
+        private void CloseTab(MUXC.TabView tabView, MUXC.TabViewItem tab)
+        {
+            //TODO: Confirm before closing the tab if its changed but unsaved
+            tabView.TabItems.Remove(tab);
+
+            // If all tabs have been closed, close the app itself
+            if (tabView.TabItems.Count == 0)
+            {
+                Application.Current.Exit();
+            }
+        }
+
         private void Tabs_Loading(FrameworkElement sender, object args)
         {
+            //TODO: Load tabs and their content, if any, from the previous session
+            var tabView = (MUXC.TabView)sender;
 
+            // If no tabs were left open in the previous session
+            // or if the app has been launched for the first time,
+            // start fresh with a new empty note at app launch
+            if (tabView.TabItems.Count == 0)
+            {
+                CreateTab(tabView);
+            }
         }
 
         private void Tabs_AddTabButtonClick(MUXC.TabView sender, object args)
         {
-
+            CreateTab(sender);
         }
 
         private void Tabs_TabCloseRequested(MUXC.TabView sender, MUXC.TabViewTabCloseRequestedEventArgs args)
         {
-
+            CloseTab(sender, args.Tab);
         }
     }
 }
